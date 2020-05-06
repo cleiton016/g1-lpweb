5ee638ec6e2dec4e6ad7e097b58190915cac6b53
cleiton016 <cleitonluiz2014@gmail.com>
2020-05-06 17:54:47 -0300

    Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,38 +16,62 @@ export class AppComponent implements OnInit {
   nota1 = null;
   nota2 = null;
 
-  registros = [];
+  selectTurma = null;
+  registros = [ ];
   // </Variaveis>
   constructor(private service: TurmasService) {}
 
   ngOnInit(): void {
-    this.service.lista()
-      .subscribe((dados: any) => this.turmas = dados);
+    this.service.lista().subscribe((dados: any) => this.turmas = dados);
   }
-  
+
+  getTurma(numero: any) {
+    this.selectTurma = this.turmas.filter((dados: any) => numero === dados.numero)[0].alunos;
+  }
 
 
   salvar(form: any) {
-    this.registros.push({turma: this.turma, aluno: this.aluno, frequencia: this.frequencia, nota1: this.nota1, nota2: this.nota2});
+    this.registros.push({numeroTurma: this.turma, codigoAluno: this.aluno,
+       frequencia: this.frequencia, nota1: this.nota1, nota2: this.nota2});
+    console.log(this.registros);
     form.reset();
   }
+
   encontrarTurma(numero: any) {
-    if (numero != null) {
-      for (const i of this.turmas) {
-        if (i.numero === numero) {
-          return i.alunos;
-        }
-      }
+    const info = this.turmas.filter((dados: any) => numero === dados.numero)[0];
+    return info.numero + ' - ' + info.nome;
   }
+  encontrarAluno(codigo: any, numero: any) {
+    const tur =  this.turmas.filter((dados: any) => numero === dados.numero)[0];
+    const aluno = tur.alunos.filter((dados: any) => codigo === dados.codigo)[0];
+    return aluno.codigo + ' - ' + aluno.nome;
   }
-  encontrarAluno(codigo: any) {
-    console.log(codigo);
-    if (codigo != null) {
-      for (const i of this.turmas) {
-        if (i.codigo === codigo) {
-          return i.alunos;
-        }
-      }
+  calcularMedia(nota1: any, nota2: any) {
+    return (nota1 + nota2) / 2;
   }
+
+  calcularFrequenciaMedia() {
+    let soma = 0;
+    for (const i of this.registros) {
+      soma += i.frequencia;
+    }
+    return soma / this.registros.length;
   }
+  calcularNota1Media() {
+    let soma = 0;
+    for (const i of this.registros) {
+      soma += i.nota1;
+    }
+    return soma / this.registros.length;
+  }
+  calcularNota2Media() {
+    let soma = 0;
+    for (const i of this.registros) {
+      soma += i.nota2;
+    }
+    return soma / this.registros.length;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
